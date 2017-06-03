1aa1dc8102cd8610580052d26ced64cb57a66453
Andy Stanford-Bluntish <andy@bluntish.net>
2017-06-03 23:36:38 +1000

    Don't override this.options
    Since upgrading, the ember-cli version, setting this.options caused
    issues build the addon. Access the addon options through the app
    instead.

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ module.exports = {
 
   included: function() {
     this._super.included.apply(this, arguments);
+    this.app.options.imagemin = this.app.options.imagemin || {};
 
-    this.enabled = this.app.env === 'production'
-    this.options = this.app.options.imagemin = this.app.options.imagemin || {};
-
-    if ('enabled' in this.options) {
-      this.enabled = this.options.enabled;
-      delete this.options.enabled;
+    if ('enabled' in this.app.options.imagemin) {
+      this.enabled = this.app.options.imagemin.enabled;
+      delete this.app.options.imagemin.enabled;
+    } else {
+      this.enabled = this.app.env === 'production';
     }
 
-    if (!this.options.plugins || this.options.plugins.length === 0) {
+    if (!this.app.options.imagemin.plugins || this.app.options.imagemin.plugins.length === 0) {
       this.enabled = false;
     }
   },
 
   postprocessTree: function(type, tree) {
     if (this.enabled) {
-      tree = new imagemin(tree, this.options);
+      tree = new imagemin(tree, this.app.options.imagemin);
     }
 
     return tree;
