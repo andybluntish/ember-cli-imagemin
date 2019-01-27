df507355132851ee9fdc26fc59cdf208ca0c8565
Andy Stanford-Bluntish <andy@bluntish.net>
2019-01-27 11:11:29 +1030

    cleanup tests

--- tests/acceptance/imagemin-test.js
+++ tests/acceptance/imagemin-test.js
@@ -1,5 +1,4 @@
 import { visit } from '@ember/test-helpers'
-import $ from 'jquery'
 import { module, test } from 'qunit'
 import { setupApplicationTest } from 'ember-qunit'
 
@@ -10,32 +9,56 @@ const uncompressedSizes = {
   svg: 4619,
 }
 
+// Compare the Content-Length of each image against the original
+// file size. Dummy app is setup to enable compression, so the
+// file sizes should be smaller.
+async function fetchImageSize(ext) {
+  const url = `/logo.${ext}`
+
+  const response = await fetch(url)
+  const body = await response.text()
+  const contentLength = await response.headers.get('Content-Length')
+
+  if (ext === 'svg') {
+    return body.length
+  } else {
+    return parseInt(contentLength, 10)
+  }
+}
+
 module('Acceptance | imagemin', function(hooks) {
   setupApplicationTest(hooks)
 
-  test('Image files are compressed', async function(assert) {
-    assert.expect(3)
+  test('JPEG files are compressed', async function(assert) {
+    assert.expect(1)
 
     await visit('/')
+    const ext = 'jpg'
+    const compressedSize = await fetchImageSize(ext)
+    const uncompressedSize = uncompressedSizes[ext]
 
-    // Compare the Content-Length of each image against the original
-    // file size. Dummy app is setup to enable compression, so the
-    // file sizes should be smaller.
-    Object.keys(uncompressedSizes).forEach(ext => {
-      const url = `/logo.${ext}`
-      const uncompressedSize = uncompressedSizes[ext]
+    assert.ok(compressedSize < uncompressedSize, `${ext} file compressed`)
+  })
 
-      $.get(url).then((data, status, xhr) => {
-        let compressedSize
+  test('PNG files are compressed', async function(assert) {
+    assert.expect(1)
 
-        if (ext === 'svg') {
-          compressedSize = xhr.responseText.length
-        } else {
-          compressedSize = parseInt(xhr.getResponseHeader('Content-Length'), 10)
-        }
+    await visit('/')
+    const ext = 'png'
+    const compressedSize = await fetchImageSize(ext)
+    const uncompressedSize = uncompressedSizes[ext]
 
-        assert.ok(compressedSize < uncompressedSize, `${ext} file compressed`)
-      })
-    })
+    assert.ok(compressedSize < uncompressedSize, `${ext} file compressed`)
+  })
+
+  test('SVG files are compressed', async function(assert) {
+    assert.expect(1)
+
+    await visit('/')
+    const ext = 'svg'
+    const compressedSize = await fetchImageSize(ext)
+    const uncompressedSize = uncompressedSizes[ext]
+
+    assert.ok(compressedSize < uncompressedSize, `${ext} file compressed`)
   })
 })
